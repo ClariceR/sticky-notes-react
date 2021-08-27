8b06ac62fcb4fe38a3de85548480db7d651c361a
clarice_r <claricerdallorto@gmail.com>
2021-08-27 13:22:10 +0100

    Finished responsive layout

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div className="container flex">
+      <div className="container flex flex--column">
         <div className="header__container">
-          <header className="app-header__title">
+          <header className="app-header__title grid grid-two">
             <h1 className="logo">
               Super
               <br />
@@ -13,43 +13,44 @@ function App() {
               <br />
               Notes
             </h1>
-            <aside className="app-header__controls flex">
+            <aside className="app-header__controls">
               <input
                 className="search"
                 type="text"
                 placeholder="Type here to search..."
               />
-              <button className="add-new">+ New Note</button>
+              <button className="add-new effect2">+ New Note</button>
             </aside>
           </header>
         </div>
-
-        <ul className="notes__list">
-          <li className="note effect2">
-            <input className="note__title" type="text" placeholder="Title" />
-            <textarea
-              className="note__description"
-              placeholder="Description..."
-            />
-            <span className="note__delete">X</span>
-          </li>
-          <li className="note effect2">
-            <input className="note__title" type="text" placeholder="Title" />
-            <textarea
-              className="note__description"
-              placeholder="Description..."
-            />
-            <span className="note__delete">X</span>
-          </li>
-          <li className="note effect2">
-            <input className="note__title" type="text" placeholder="Title" />
-            <textarea
-              className="note__description"
-              placeholder="Description..."
-            />
-            <span className="note__delete">X</span>
-          </li>
-        </ul>
+        <div className="notes__list--wrapper grid">
+          <ul className="notes__list">
+            <li className="note effect2">
+              <input className="note__title" type="text" placeholder="Title" />
+              <textarea
+                className="note__description"
+                placeholder="Description..."
+              />
+              <span className="note__delete">x</span>
+            </li>
+            <li className="note effect2">
+              <input className="note__title" type="text" placeholder="Title" />
+              <textarea
+                className="note__description"
+                placeholder="Description..."
+              />
+              <span className="note__delete">x</span>
+            </li>
+            <li className="note effect2">
+              <input className="note__title" type="text" placeholder="Title" />
+              <textarea
+                className="note__description"
+                placeholder="Description..."
+              />
+              <span className="note__delete">x</span>
+            </li>
+          </ul>
+        </div>
       </div>
     </>
   );
